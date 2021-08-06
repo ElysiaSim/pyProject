a53a9397a0bfa90aeaf4367ad5ef0044268f506c
ocvb <electrokai1@gmail.com>
2021-08-07 04:11:24 +0800

    UIpadate

--- dataExcel.py
+++ dataExcel.py
@@ -1,4 +1,5 @@
 import pandas as pd
+# Install 'python3 -m pip install --user xlrd'
 data = pd.read_excel("dataNew.xls")
 data['Period'] = data['Period'].str.replace('Jan', '')
 #df = data[(data["Period"] >= 1900) & (data["Period"] <= 1930)]
