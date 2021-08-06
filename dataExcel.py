373d41f9e5b1bdca8106b08ac0c4c431534e2b9c
ocvb <electrokai1@gmail.com>
2021-08-07 04:08:38 +0800

    update

--- dataExcel.py
+++ dataExcel.py
@@ -1,4 +1,5 @@
 import pandas as pd
 data = pd.read_excel("dataNew.xls")
-data[(data["Period"] >= 1900) & (data["Period"] <= 1930)]
+data['Period'] = data['Period'].str.replace('Jan', '')
+#df = data[(data["Period"] >= 1900) & (data["Period"] <= 1930)]
 print(data)
