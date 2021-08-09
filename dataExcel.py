095ea8da448677ddc02ee5016b02cffc92aac24a
ocvb <electrokai1@gmail.com>
2021-08-09 21:51:51 +0800

    Updated

--- dataExcel.py
+++ dataExcel.py
@@ -1,6 +1,27 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 # Install 'python3 -m pip install --user xlrd'
 data = pd.read_excel("dataNew.xls")
+
 data['Period'] = data['Period'].str.replace('Jan', '')
-#df = data[(data["Period"] >= 1900) & (data["Period"] <= 1930)]
+data["Period"] = data["Period"].astype(int)
+# data = data.loc[(data.Period <= 1910)]
+data = data[(data["Period"] >= 1900) & (data["Period"] <= 1910)]
 print(data)
+
+
+ps = data['Calories']
+index = data['Period']
+plt.xlabel('Year', fontsize=5)
+plt.ylabel('No. of Calories', fontsize=8)
+plt.xticks(ps.index, index, fontsize=10, rotation=90)
+plt.title('1900 - 1910')
+plt.bar(ps.index, ps.values)
+plt.show()
+
+"""for column in data[['Calories']]:
+    columnValues = data[column]
+    print(column)
+    print(columnValues.values)"""
+    
