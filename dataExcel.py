1f1a51a2c7d21fa4a79648bb82d55878eb4c8242
ocvb <electrokai1@gmail.com>
2021-08-10 12:07:05 +0800

    Updated

--- dataExcel.py
+++ dataExcel.py
@@ -1,21 +1,52 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import xlrd
 # Install 'python3 -m pip install --user xlrd'
 data = pd.read_excel("dataNew.xls")
-
 data['Period'] = data['Period'].str.replace('Jan', '')
 data["Period"] = data["Period"].astype(int)
-# data = data.loc[(data.Period <= 1910)]
-data = data[(data["Period"] >= 1900) & (data["Period"] <= 1910)]
+
+
+
+#data = data[(data["Period"] >= 1900) & (data["Period"] <= 1910)]
+data = data.loc[(data.Period >= 1900) & (data.Period <= 1910)]
 print(data)
-
-
 ps = data['Calories']
 index = data['Period']
 plt.xlabel('Year', fontsize=5)
 plt.ylabel('No. of Calories', fontsize=8)
 plt.xticks(ps.index, index, fontsize=10, rotation=90)
 plt.title('1900 - 1910')
+plt.bar(ps.index, ps.values)
+plt.show()
+
+
+data = pd.read_excel("dataNew.xls")
+data['Period'] = data['Period'].str.replace('Jan', '')
+data["Period"] = data["Period"].astype(int)
+data = data.loc[(data.Period > 1910) & (data.Period <= 1920)]
+print(data)
+ps = data['Calories']
+index = data['Period']
+plt.xlabel('Year', fontsize=5)
+plt.ylabel('No. of Calories', fontsize=8)
+plt.xticks(ps.index, index, fontsize=10, rotation=90)
+plt.title('1911 - 1920')
+plt.bar(ps.index, ps.values)
+plt.show()
+
+
+data = pd.read_excel("dataNew.xls")
+data['Period'] = data['Period'].str.replace('Jan', '')
+data["Period"] = data["Period"].astype(int)
+data = data[(data["Period"] > 1920) & (data["Period"] <= 1930)]
+print(data)
+ps = data['Calories']
+index = data['Period']
+plt.xlabel('Year', fontsize=5)
+plt.ylabel('No. of Calories', fontsize=8)
+plt.xticks(ps.index, index, fontsize=10, rotation=90)
+plt.title('1921 - 1930')
 plt.bar(ps.index, ps.values)
 plt.show()
