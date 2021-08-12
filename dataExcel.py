2a790d34e70c1e5afae32a8b0024156af7fe9d77
ocvb <electrokai1@gmail.com>
2021-08-12 17:35:41 +0800

    NEWNW

--- dataExcel.py
+++ dataExcel.py
@@ -15,24 +15,20 @@ newData.insert(0, 'Years', moveColumn) """
 del newData["Period"]
 print(newData)
 
-
-newData_sorted = newData.sort_values(by=["Calories"], ascending=True)
 newData1 = newData.loc[(newData.Years >= 1900) & (newData.Years <= 1910)]
 newData2 = newData.loc[(newData.Years >= 1911) & (newData.Years <= 1920)]
 newData3 = newData.loc[(newData.Years >= 1921) & (newData.Years <= 1930)] 
 
-
 def first():
     print("Total: ", newData1["Calories"].sum())
     print("Mean: ", newData1["Calories"].mean())
-    newData_sorted = newData.sort_values(by="Calories", ascending=True)
-    ps = newData1['Calories']
+    ps = newData1['Calories'].sort_values(ignore_index=True)
     index = newData1['Years']
     plt.xlabel('Year', fontsize=5)
     plt.ylabel('No. of Calories', fontsize=8)
-    plt.xticks(newData_sorted.index, index, fontsize=10, rotation=90)
+    plt.xticks(ps.index, index, fontsize=10, rotation=90)
     plt.title('1900 - 1910')
-    plt.bar(newData_sorted.index, newData_sorted.values)
+    plt.bar(ps.index, ps.values)
     plt.savefig("1900 - 1910")
     plt.show()
 
@@ -40,7 +36,7 @@ def first():
 def second():
     print("Total: ", newData2["Calories"].sum())
     print("Mean: ", newData2["Calories"].mean())
-    ps = newData2['Calories']
+    ps = newData2['Calories'].sort_values(ignore_index=True)
     index = newData2['Years']
     plt.xlabel('Year', fontsize=5)
     plt.ylabel('No. of Calories', fontsize=8)
@@ -54,7 +50,7 @@ def second():
 def third():
     print("Total: ", newData3["Calories"].sum())
     print("Mean: ", newData3["Calories"].mean())
-    ps = newData3["Calories"]
+    ps = newData3["Calories"].sort_values(ignore_index=True)
     index = newData3['Years']
     plt.xlabel('Year', fontsize=5)
     plt.ylabel('No. of Calories', fontsize=8)
