e48fbedfd32861cd94c95f3afd2119da7ff23647
ocvb <electrokai1@gmail.com>
2021-08-12 16:22:44 +0800

    ewf

--- dataExcel.py
+++ dataExcel.py
@@ -9,50 +9,54 @@ data = pd.read_excel("dataNew.xls")
 data['Period'] = data['Period'].str.replace('Jan', '')
 data["Period"] = data["Period"].astype(int)
 #data = data[(data["Period"] >= 1900) & (data["Period"] <= 1910)]
-
-
-
 data = data.loc[(data.Period >= 1900) & (data.Period <= 1910)]
-print("Total: ", data["Calories"].sum())
-print("Mean: ", data["Calories"].mean())
-ps = data['Calories']
-index = data['Period']
-plt.xlabel('Year', fontsize=5)
-plt.ylabel('No. of Calories', fontsize=8)
-plt.xticks(ps.index, index, fontsize=10, rotation=90)
-plt.title('1900 - 1910')
-plt.bar(ps.index, ps.values)
-plt.savefig("1900 - 1910")
-plt.show()
+#data = data.loc[(data.Period >= 1911) & (data.Period <= 1920)]
+#data = data[(data["Period"] > 1920) & (data["Period"] <= 1930)]
+
+def first():
+    print("Total: ", data["Calories"].sum())
+    print("Mean: ", data["Calories"].mean())
+    ps = data['Calories']
+    index = data['Period']
+    plt.xlabel('Year', fontsize=5)
+    plt.ylabel('No. of Calories', fontsize=8)
+    plt.xticks(ps.index, index, fontsize=10, rotation=90)
+    plt.title('1900 - 1910')
+    plt.bar(ps.index, ps.values)
+    plt.savefig("1900 - 1910")
+    plt.show()
 
 
 
-"""
-data = data.loc[(data.Period >= 1911) & (data.Period <= 1920)]
-print("Total: ", data["Calories"].sum())
-print("Mean: ", data["Calories"].mean())
-ps = data['Calories']
-index = data['Period']
-plt.xlabel('Year', fontsize=5)
-plt.ylabel('No. of Calories', fontsize=8)
-plt.xticks(ps.index, index, fontsize=10, rotation=90)
-plt.title('1911 - 1920')
-plt.bar(ps.index, ps.values)
-plt.savefig("1911 - 1920")
-plt.show()
+
+def seoncd():
+    print("Total: ", data["Calories"].sum())
+    print("Mean: ", data["Calories"].mean())
+    ps = data['Calories']
+    index = data['Period']
+    plt.xlabel('Year', fontsize=5)
+    plt.ylabel('No. of Calories', fontsize=8)
+    plt.xticks(ps.index, index, fontsize=10, rotation=90)
+    plt.title('1911 - 1920')
+    plt.bar(ps.index, ps.values)
+    plt.savefig("1911 - 1920")
+    plt.show()
+
+
+def third():
+    print("Total: ", data["Calories"].sum())
+    print("Mean: ", data["Calories"].mean())
+    ps = data['Calories']
+    index = data['Period']
+    plt.xlabel('Year', fontsize=5)
+    plt.ylabel('No. of Calories', fontsize=8)
+    plt.xticks(ps.index, index, fontsize=10, rotation=90)
+    plt.title('1921 - 1930')
+    plt.bar(ps.index, ps.values)
+    plt.savefig("1921 - 1930")
+    plt.show()
 
 
 
-data = data[(data["Period"] > 1920) & (data["Period"] <= 1930)]
-print(data)
-print("Total: ", data["Calories"].sum())
-print("Mean: ", data["Calories"].mean())
-ps = data['Calories']
-index = data['Period']
-plt.xlabel('Year', fontsize=5)
-plt.ylabel('No. of Calories', fontsize=8)
-plt.xticks(ps.index, index, fontsize=10, rotation=90)
-plt.title('1921 - 1930')
-plt.bar(ps.index, ps.values)
-plt.savefig("1921 - 1930")
-plt.show() """
+if __name__ == "__main__":
+    first()
