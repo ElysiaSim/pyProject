85ec5312201f22a5c25ae110e4cf9651dc0ddffb
ocvb <electrokai1@gmail.com>
2021-08-17 14:15:46 +0800

    ee

--- dataExcel.py
+++ dataExcel.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 data = pd.read_excel("dataNew.xls")
 data1 = data["Period"].str.split(" ", n = 1, expand = True)
 newData = data.assign(Years = data1[1])
-newData["Years"] = newData["Years"].astype(int)
+newData["Years"] = newData["Years"].astype(str)
 newData.index = newData["Years"]
 """
 moveColumn = newData.pop("Years")
@@ -14,20 +14,20 @@ newData.insert(0, 'Years', moveColumn) """
 del newData["Period"]
 print(newData)
 # newData1 = newData[(newData["Years"] >= 1900) & (newData["Years"] <= 1910)]
-newData1 = newData.loc[(newData.Years >= 1900) & (newData.Years <= 1910)]
-newData2 = newData.loc[(newData.Years >= 1911) & (newData.Years <= 1920)]
-newData3 = newData.loc[(newData.Years >= 1921) & (newData.Years <= 1930)] 
+newData1 = newData.loc[(newData.Years >= str(1900)) & (newData.Years <= str(1910))]
+newData2 = newData.loc[(newData.Years >= str(1911)) & (newData.Years <= str(1920))]
+newData3 = newData.loc[(newData.Years >= str(1921)) & (newData.Years <= str(1930))] 
 
 def first():
     print(newData1.head(3))
     print("First:")
     print("Total: ", round(newData1["Calories"].sum(), 2))
     print("Mean: ", round(newData1["Calories"].mean(), 2))
-    y = newData1['Calories']
+    y = newData1['Calories'].sort_values(ascending= True)
     x = np.arange(len(y.index))
     plt.xlabel('Year', fontsize=13)
     plt.ylabel('No. of Calories', fontsize=8)
-    plt.xticks(y.index, x, fontsize=11, rotation=0)
+    plt.xticks(x, y.index, fontsize=11, rotation=0)
     plt.title('1900 - 1910')
     plt.bar(y.index, y.values)
     plt.savefig("1900 - 1910")
